843ec458d878930139a8568ce75bceb59e1b495d
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-06-21 08:27:03 +0800

    Create drawingface.js

---
+++ 2024-6/drawingface.js
@@ -0,0 +1,12 @@
+  function setup() {
+            createCanvas(400, 400);
+        }
+
+        function draw() {
+            background(220);
+            ellipse(212, 206, 283, 318); // head x,y,size
+			ellipse(212, 250, 100, 73); // mouth x,y,size
+			ellipse(150, 150, 30, 30); // left eye x,y,size
+			ellipse(278, 150, 30, 30); // right eye x,y,size
+        }
+    </script>
